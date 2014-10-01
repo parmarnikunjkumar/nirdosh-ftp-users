1eb36e6f654425f08d6c3e7f70e64f9f3cd1f789
nirdosh <n.parmar@meteogroup.de>
2014-10-01 20:07:36 +0200

    UserDAO added

---
+++ src/main/java/com/nirdosh/db/dao/UserDAO.java
@@ -0,0 +1,9 @@
+package com.nirdosh.db.dao;
+
+import com.nirdosh.domain.User;
+
+public interface UserDAO {
+	
+	User get(double id);
+	
+}
---
+++ src/main/java/com/nirdosh/db/dao/impl/UserDAOImpl.java
@@ -0,0 +1,13 @@
+package com.nirdosh.db.dao.impl;
+
+import com.nirdosh.db.dao.UserDAO;
+import com.nirdosh.domain.User;
+
+public class UserDAOImpl implements UserDAO{
+
+	public User get(double id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
